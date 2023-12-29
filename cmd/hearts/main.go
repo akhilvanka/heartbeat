3da372243eb5fc3f3574c77173a9f4cb2962b7c2
Akhil Vanka <avanka2005@gmail.com>
2023-12-29 13:59:27 -0500

    TODO: Finished client, move everything to named functions and properly release resources

--- cmd/hearts/main.go
+++ cmd/hearts/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"heartbeat/client"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,14 +17,26 @@ import (
 )
 
 var (
-	isServer bool // Makeshift variable name for operations now
+	isServer  bool // Makeshift variable name for operations now
+	serverURL string
 )
+
+type Payload struct {
+	SystemStatus int
+	Services     []Domain
+}
+
+type Domain struct {
+	ServiceName   string
+	ServiceStatus int
+}
 
 func main() {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 
 	isServer = false
+	serverURL = "http://lcoalhost:3000"
 
 	signalChange := make(chan os.Signal, 1)
 	signal.Notify(signalChange, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
@@ -55,6 +71,7 @@ func main() {
 func run(ctx context.Context, out io.Writer) error {
 
 	log.SetOutput(out)
+	var statusList []Domain
 
 	if isServer {
 		return nil
@@ -72,9 +89,39 @@ func run(ctx context.Context, out io.Writer) error {
 					status := client.QueryURL(fmt.Sprintf("https://%s", domain))
 					if status {
 						log.Printf("Domain %s is up", domain)
+						a := Domain{
+							ServiceName:   domain,
+							ServiceStatus: 1,
+						}
+						statusList = append(statusList, a)
 					} else {
 						log.Printf("Domain %s is down", domain)
+						a := Domain{
+							ServiceName:   domain,
+							ServiceStatus: 0,
+						}
+						statusList = append(statusList, a)
 					}
+				}
+				payloadMessage := Payload{
+					SystemStatus: 1,
+					Services:     statusList,
+				}
+				body, _ := json.Marshal(payloadMessage)
+
+				// Send JSON status
+				resp, err := http.Post(serverURL, "application/json", bytes.NewBuffer(body))
+				if err != nil {
+					return err
+				}
+				defer func(Body io.ReadCloser) {
+					err := Body.Close()
+					if err != nil {
+						log.Printf("Error found when closing body")
+					}
+				}(resp.Body) // Handle this and check if it actually leaks (Shouldn't but just to be safe)
+				if resp.StatusCode != 200 {
+					return errors.New("unable to post data to server")
 				}
 			}
 		}
