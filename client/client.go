aca0eb9b9ceb8803b05459254c26cb6d6383b162
Akhil Vanka <dev+github@akhilv.me>
2023-12-29 19:09:24 -0500

    Quality of life updates, and completed client side application

--- client/client.go
+++ client/client.go
@@ -1,11 +1,27 @@
 package client
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 )
+
+type Payload struct {
+	SystemStatus int
+	Services     []Domain
+}
+
+type Domain struct {
+	ServiceName   string
+	ServiceStatus int
+}
 
 func ReadCaddyfile() ([]string, error) {
 	fileContent, err := os.ReadFile("/etc/Caddyfile") // Read the text from my caddyFile
@@ -14,6 +30,7 @@ func ReadCaddyfile() ([]string, error) {
 		return nil, err
 	}
 	text := string(fileContent)
+
 	// Define the regular expression pattern
 	pattern := regexp.MustCompile(`(?m)\b([a-zA-Z0-9.-]+)\s*{`)
 
@@ -23,7 +40,7 @@ func ReadCaddyfile() ([]string, error) {
 	// Extract the matched domain names
 	var domains []string
 	for _, match := range matches {
-		if len(match) > 1 {
+		if len(match) > 1 && strings.IndexByte(match[1], '.') != -1 {
 			domains = append(domains, match[1])
 		}
 	}
@@ -31,9 +48,56 @@ func ReadCaddyfile() ([]string, error) {
 }
 
 func QueryURL(url string) bool {
-	_, err := http.Get(url)
-	if err != nil {
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode != 200 {
 		return false
 	}
 	return true
+}
+
+func CollectionRun(serverURL string) error {
+	var statusList []Domain
+	data, err := ReadCaddyfile()
+	if err != nil {
+		return err
+	}
+	for _, domain := range data {
+		status := QueryURL(fmt.Sprintf("https://%s", domain))
+		if status {
+			log.Printf("Domain %s is up", domain)
+			a := Domain{
+				ServiceName:   domain,
+				ServiceStatus: 1,
+			}
+			statusList = append(statusList, a)
+		} else {
+			log.Printf("Domain %s is down", domain)
+			a := Domain{
+				ServiceName:   domain,
+				ServiceStatus: 0,
+			}
+			statusList = append(statusList, a)
+		}
+	}
+	payloadMessage := Payload{
+		SystemStatus: 1,
+		Services:     statusList,
+	}
+	body, _ := json.Marshal(payloadMessage)
+
+	// Send JSON status
+	resp, err := http.Post(serverURL, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Printf("Error found when closing body")
+		}
+	}(resp.Body) // Handle this and check if it actually leaks (Shouldn't but just to be safe)
+	if resp.StatusCode != 200 {
+		return errors.New("unable to post data to server")
+	}
+	return nil
 }
--- cmd/hearts/main.go
+++ cmd/hearts/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"heartbeat/client"
 	"io"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -20,16 +15,6 @@ var (
 	isServer  bool // Makeshift variable name for operations now
 	serverURL string
 )
-
-type Payload struct {
-	SystemStatus int
-	Services     []Domain
-}
-
-type Domain struct {
-	ServiceName   string
-	ServiceStatus int
-}
 
 func main() {
 	ctx := context.Background()
@@ -69,9 +54,7 @@ func main() {
 }
 
 func run(ctx context.Context, out io.Writer) error {
-
 	log.SetOutput(out)
-	var statusList []Domain
 
 	if isServer {
 		return nil
@@ -80,48 +63,11 @@ func run(ctx context.Context, out io.Writer) error {
 			select {
 			case <-ctx.Done():
 				return nil
-			case <-time.Tick(1 * time.Second):
-				data, err := client.ReadCaddyfile()
+			case <-time.Tick(120 * time.Second):
+				log.Printf("Running data collection")
+				err := client.CollectionRun(serverURL)
 				if err != nil {
 					return err
-				}
-				for _, domain := range data {
-					status := client.QueryURL(fmt.Sprintf("https://%s", domain))
-					if status {
-						log.Printf("Domain %s is up", domain)
-						a := Domain{
-							ServiceName:   domain,
-							ServiceStatus: 1,
-						}
-						statusList = append(statusList, a)
-					} else {
-						log.Printf("Domain %s is down", domain)
-						a := Domain{
-							ServiceName:   domain,
-							ServiceStatus: 0,
-						}
-						statusList = append(statusList, a)
-					}
-				}
-				payloadMessage := Payload{
-					SystemStatus: 1,
-					Services:     statusList,
-				}
-				body, _ := json.Marshal(payloadMessage)
-
-				// Send JSON status
-				resp, err := http.Post(serverURL, "application/json", bytes.NewBuffer(body))
-				if err != nil {
-					return err
-				}
-				defer func(Body io.ReadCloser) {
-					err := Body.Close()
-					if err != nil {
-						log.Printf("Error found when closing body")
-					}
-				}(resp.Body) // Handle this and check if it actually leaks (Shouldn't but just to be safe)
-				if resp.StatusCode != 200 {
-					return errors.New("unable to post data to server")
 				}
 			}
 		}
